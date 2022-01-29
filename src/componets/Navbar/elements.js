4ead4de0edeb56732747adb786e319d7838e8dcf
Hermes222 <54920835+Hermes222@users.noreply.github.com>
2022-01-29 10:29:03 -0800

    Updates

--- src/componets/Navbar/elements.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const elements = () => {
-  return <div></div>;
-};
-
-export default elements;
--- src/componets/Navbar/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Navbar =() =>{
-    return (<div>
-        <p>Hello everybody</p>
-    </div>);
-}
-
-export default Navbar;
